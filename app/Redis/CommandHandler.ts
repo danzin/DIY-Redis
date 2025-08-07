64bf7cead0166f4f959afe7ad2bb6534fd8a1ece
danzin <excl.d.k@gmail.com>
2025-08-07 11:03:37 +0300

    feat: XREVRANGE

--- app/Redis/CommandHandler.ts
+++ app/Redis/CommandHandler.ts
@@ -465,6 +465,46 @@ export class CommandHandler {
 		return toRESPEntryArray(result);
 	}
 
+	async xrevrange(args: string[]): Promise<string> {
+		if (args.length < 3) {
+			return simpleErrorResponse("wrong number of arguments for 'xrevrange' command");
+		}
+
+		const [streamKey, endId, startId, ...rest] = args;
+		let count: number | undefined = undefined;
+
+		// Parse the optional COUNT argument
+		if (rest.length > 0) {
+			if (rest[0]?.toUpperCase() === "COUNT" && rest[1]) {
+				count = parseInt(rest[1], 10);
+				if (isNaN(count) || count < 0) {
+					return simpleErrorResponse("value is not an integer or out of range");
+				}
+			} else {
+				return simpleErrorResponse("syntax error");
+			}
+		}
+
+		const storedStream = this.redisStore.get(streamKey);
+
+		// If no stram or not a stream, return an empty array
+		if (!storedStream || storedStream.type !== "stream") {
+			return toRESPEntryArray([]);
+		}
+
+		const streamEntries = JSON.parse(storedStream.value) as StreamEntry[];
+
+		const filteredEntries = getEntryRange(streamEntries, startId, endId);
+
+		// reverse the array of results.
+		const reversedEntries = filteredEntries.reverse();
+
+		// Apply the COUNT limit if it was provided.
+		const finalEntries = count !== undefined ? reversedEntries.slice(0, count) : reversedEntries;
+
+		return toRESPEntryArray(finalEntries);
+	}
+
 	config(args: string[]): string {
 		const subCommand = args[0]?.toLowerCase();
 		const parameter = args[1]?.toLowerCase();
